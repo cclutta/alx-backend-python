e491a99525120900d2c7ec3e8d605ad0307f1d94
cclutta <chelsea.lutta@gmail.com>
2023-01-05 12:06:34 +0300

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+add module
+
+Contains one function add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Returns the sum of arguments as float. """
+    return a + b
