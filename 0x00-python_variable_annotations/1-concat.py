fb74fcfc2cd214e83cc7c595b9019b8dae08c105
cclutta <chelsea.lutta@gmail.com>
2023-01-05 12:11:21 +0300

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+concat module
+
+Contains one function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns the sum of arguments as float. """
+    return str1 + str2
