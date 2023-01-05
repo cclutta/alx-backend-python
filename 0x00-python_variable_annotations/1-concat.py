c62c3794825b0cf90218e2531e9b981a838c4272
cclutta <chelsea.lutta@gmail.com>
2023-01-05 12:17:26 +0300

    Task 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,7 +5,6 @@ concat module
 Contains one function concat
 """
 
-
 def concat(str1: str, str2: str) -> str:
-    """ Returns the sum of arguments as float. """
+    """ Returns concatenated strings. """
     return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+floor module
+
+Contains one function floor
+"""
+from math import floor
+
+
+def floor(n: float) -> int:
+    """ Returns the sum of arguments as float. """
+    return int(n)
