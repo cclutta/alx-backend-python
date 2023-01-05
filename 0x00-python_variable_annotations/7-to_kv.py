1515fc94d6d3fb9a32892632a4c75bc746a1162f
cclutta <chelsea.lutta@gmail.com>
2023-01-05 12:45:43 +0300

    Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@ Contains function to_kv
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int,float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ returns a k,v. """
     v_1: float = v ** 2
     return (k, v_1)
