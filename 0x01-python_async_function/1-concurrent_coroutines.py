3b800aed7eb32217422919f71e9208979e36746f
cclutta <chelsea.lutta@gmail.com>
2023-01-09 21:04:36 +0300

    Task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,7 @@ from asyncio import gather
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay) -> List[float]:
     """will spawn wait_random n times with the specified max_delay. """
     return sorted(await gather(*[wait_random(max_delay) for i in range(n)]))
