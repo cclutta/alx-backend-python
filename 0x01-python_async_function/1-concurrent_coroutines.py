abeb76efe4c24e6be58226e1936820a52d780095
cclutta <chelsea.lutta@gmail.com>
2023-01-09 20:56:23 +0300

    Task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 """
-Concurrent coroutines module.
-
-Contains function wait_n
+This module contains an asynchronous coroutine.
 """
-from asyncio import gather
+
 from typing import List
+from asyncio import gather
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """will spawn wait_random n times with the specified max_delay. """
+    """
+    Args:
+        n: The number of times to generate a random value.
+        max_delay: The maximum random time generated.
+    Returns:
+        The random generated values list.
+    """
+
     return sorted(await gather(*[wait_random(max_delay) for i in range(n)]))
