47c5d4ba0eb70d32bbc3671a8aa545e2f095c3cd
cclutta <chelsea.lutta@gmail.com>
2023-01-09 20:51:55 +0300

    Task 1

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """
-Concurrent coroutines module.
+Measure runtime module.
 
-Contains function wait_n
+Contains function measure_runtime
 """
 from asyncio import gather
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+
+async def wait_n(n: int, max_delay) -> List[float]:
     """will spawn wait_random n times with the specified max_delay. """
     return sorted(await gather(*[wait_random(max_delay) for i in range(n)]))
