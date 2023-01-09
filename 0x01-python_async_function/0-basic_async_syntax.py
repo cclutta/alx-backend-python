a99b39c35f109e371d9c28461223615750a706a5
cclutta <chelsea.lutta@gmail.com>
2023-01-09 20:40:17 +0300

    Update

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,9 +7,9 @@ Contains function wait_random
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """that takes in an integer argument and waits for a random delay. """
     rand: float = random.randint(0, max_delay)
     await asyncio.sleep(rand)
     return rand
-
